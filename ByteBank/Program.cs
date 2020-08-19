334f0eca08cf647e912536a40a8376ac87d5d670
Pedro Schiavom <pedro_indalecio@hotmail.com>
2020-08-18 22:36:36 -0300

    Testando exceção divisão por zero

---
+++ ByteBank/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        int resultado = Metodo();
+        
+        if (resultado == -2)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+            }
+        Console.ReadLine();
+    }
+    //Teste com a cadeia de chamada:
+    //Metodo -> TestaDivisao -> Dividir
+        private static int Metodo()
+        {
+            int resultadoDivisao = TestaDivisao(0);
+
+            if(resultadoDivisao == -2)
+            {
+                return -2;
+            }
+
+            int resultado2 = TestaDivisao(20);
+
+            if(resultado2 == -2)
+            {
+                return -2;
+            }
+            return 0;
+        }
+
+        private static int TestaDivisao(int divisor)
+        {
+                int resultado = Dividir(10, divisor);
+
+            if(resultado == -2)
+            {
+                return -2;
+            }
+
+            if(resultado == -1)
+            {
+                Console.WriteLine("Não é possível fazer divisão por 0");
+            }
+            else
+            {
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+            }
+
+            return 0;
+            
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            if(divisor == 0)
+            {
+                return -1;
+            }
+
+            if(divisor > numero)
+            {
+                return -2;
+            }
+
+            return numero / divisor;
+        }
+    }
+}
+
+
+//namespace ByteBank
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            ContaCorrente conta = new ContaCorrente(7480, 874150);
+
+//            Console.WriteLine(ContaCorrente.TaxaOperacao);
+//            Console.ReadLine();
+//        }
+//    }
+//}
