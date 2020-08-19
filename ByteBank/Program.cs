236f45333fc4d80724183cac1bdfa414d91a129d
Pedro Schiavom <pedro_indalecio@hotmail.com>
2020-08-19 00:04:47 -0300

    Tratando exceção apenas com Exception

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -14,11 +14,16 @@ namespace ByteBank
             {
                 Metodo();
             }
-            catch(NullReferenceException erro)
+            catch(DivideByZeroException erro)
             {
+                Console.WriteLine("Não é possível divisão por zero.");
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
                 Console.WriteLine(erro.StackTrace);
                 Console.WriteLine("Aconteceu um erro!");
-                
+
             }
 
             Console.ReadLine();
@@ -33,19 +38,10 @@ namespace ByteBank
 
         private static void TestaDivisao(int divisor)
         {
-            try
-            {
+  
                 int resultado = Dividir(10, divisor);
                 Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
-            }
-            catch (DivideByZeroException erro)
-            {
-                Console.WriteLine(erro.Message);
-                Console.WriteLine(erro.StackTrace);
-                Console.WriteLine("Não é possível fazer uma divisão por zero");
-            }
-
-            Console.ReadLine();
+ 
         }
 
         private static int Dividir(int numero, int divisor)
